479299db380773766516919d17ca47bfe38b49aa
Max Taggart <max.taggart@atroposhealth.com>
2025-10-30 14:10:13 -0600

    Updating the run script.

--- run_healthbench.sh
+++ run_healthbench.sh
@@ -1,8 +1,7 @@
 #! /bin/bash
 
 uv run python -m src.simple_evals.simple_evals \
-    --output-dir /Users/max/Developer/repos/simple-evals/results/9252fc34f9bc391262e8b71138ee3b86e7d0ad7a/llama-3.1-8b \
+    --output-dir /Users/max/Developer/repos/simple-evals/results/9eb82b46b5ef7faf6ec102a989421543e84d4228/llama-3.1-8b \
     --n-threads 10 \
     --eval healthbench \
-    --model llama-3.1-8b \
-    --examples 3
+    --model llama-3.1-8b 
